c31dd048a804e583f01565e5d3569d4770136f0e
aladdineng <aladdin.enbeh@gmail.com>
2024-04-28 19:54:07 +0300

    calculate BMI

--- index.js
+++ index.js
@@ -8,4 +8,28 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+
+
+let calculateWeit = (weight /( (height/100)**2));
+alert(calculateWeit)
+
+if(calculateWeit > 18.5) {
+  alert.log("Underweight")
+} else if (calculateWeit >= 18.5 || calculateWeit <= 24.9){
+
+  alert.log("Healthy Weight")
+}else if (calculateWeit >= 25.0 || calculateWeit <= 29.9){
+
+  alert.log("Overweight")
 }
+
+else if (calculateWeit > 30){
+
+  alert.log("Above Obesity")
+}
+}
+
+
+
+
+
